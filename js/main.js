4ae2c2e205bb0bb3efac12f39099fa55f5944478
Sergey Kocharyan <loki.dogmat@mail.ru>
2022-06-21 04:18:11 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const TYPE_OF_HOUSING = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const CHECKIN = ['12:00', '13:00', '14:00'];
-const CHECKOUT = ['12:00', '13:00', '14:00'];
+const CHECK_IN_OUT = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = [
     'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 
@@ -11,7 +10,7 @@ const SIMILAR_AD_COUNT = 10;
 
 let getRandomInteger = (min, max) => {
     if (min <0 || max <0) {
-      return console.log('Числа должны быть положительные.');  
+        throw new Error('Числа должны быть положительные.');  
     };
     if (max <= min) {
       [max, min] = [min, max];  
@@ -19,7 +18,7 @@ let getRandomInteger = (min, max) => {
     let rand = min + Math.floor(Math.random() * (max - min + 1));
     return rand;
   };
-  let getRandomFloat = (min, max, numberOfSigns) => {
+let getRandomFloat = (min, max, numberOfSigns) => {
     if (min <0 || max <0) {
       return console.log('Числа должны быть положительные.');
     };
@@ -30,38 +29,44 @@ let getRandomInteger = (min, max) => {
     return ( Math.round(rand*Math.pow(10, numberOfSigns))/ Math.pow(10, numberOfSigns) )  
   };
 
-const creatAd = () => {
-    return {
-    author: getAuthor(),
-    offer: getOffer(),
-    location: getLocation(),
-    };
-};
-
 const getAuthor = () => {
+    const resultNum = getRandomInteger(1, SIMILAR_AD_COUNT);
+    if (resultNum < SIMILAR_AD_COUNT) {
+        return {
+            avatar: `img/avatars/user0${resultNum}.png`
+        };
+    }
     return {
-    avatar: `img/avatars/user${getRandomInteger(0, 10)}.png`,
-    };
-};
-const getOffer = () => {
-    return {
-    title: 'Взгляни-ка!',
-    address: 'location.lat, location.lng',
-    price: getRandomInteger(0,),
-    type: TYPE_OF_HOUSING[getRandomInteger(0, TYPE_OF_HOUSING.length-1)],
-    rooms: getRandomInteger(0,),
-    guests: getRandomInteger(0,),
-    checkin: CHECKIN[getRandomInteger(0, CHECKIN.length-1)],
-    chechout: CHECKOUT[getRandomInteger(0, CHECKOUT.length-1)],
-    features: FEATURES.slice(getRandomInteger(getRandomInteger(0, FEATURES.length-1), getRandomInteger(0, FEATURES.length-1))),
-    description: 'Чисто, красиво, душевно',
-    photos: PHOTOS.slice(getRandomInteger(getRandomInteger(0, PHOTOS.length-1), getRandomInteger(0, PHOTOS.length-1))),
+    avatar: `img/avatars/user${resultNum}.png`,
     };
 };
 const getLocation = () => {
     return {
     lat: getRandomFloat(35.65000, 35.7000, 5),
     lng: getRandomFloat(139.70000, 139.80000, 5),
+    };
+};
+const getOffer = () => {
+    return {
+    title: 'Взгляни-ка!',
+    address: `${getRandomFloat(35.65000, 35.7000, 5)}, ${getRandomFloat(139.70000, 139.80000, 5)}`,
+    price: getRandomInteger(0, 100000),
+    type: TYPE_OF_HOUSING[getRandomInteger(0, TYPE_OF_HOUSING.length-1)],
+    rooms: getRandomInteger(1, 10),
+    guests: getRandomInteger(0, 12),
+    checkin: CHECK_IN_OUT[getRandomInteger(0, CHECK_IN_OUT.length-1)],
+    chechout: CHECK_IN_OUT[getRandomInteger(0, CHECK_IN_OUT.length-1)],
+    features: FEATURES.slice(getRandomInteger(getRandomInteger(0, FEATURES.length-1), getRandomInteger(0, FEATURES.length-1))),
+    description: 'Чисто, красиво, душевно',
+    photos: PHOTOS.slice(getRandomInteger(getRandomInteger(0, PHOTOS.length-1), getRandomInteger(0, PHOTOS.length-1))),
+    };
+};
+
+const creatAd = () => {
+    return {
+    author: getAuthor(),
+    offer: getOffer(),
+    location: getLocation(),
     };
 };
 
